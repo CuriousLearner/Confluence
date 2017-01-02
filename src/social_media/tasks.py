fb4c29a6b5c25b0de14c3ff1d4fc262ef718013b
shashankaryan <aryanshashank31@gmail.com>
2017-01-02 20:14:37 +0530

    Celery task added to post on fb page

--- src/social_media/tasks.py
+++ src/social_media/tasks.py
@@ -1,20 +1,18 @@
 # Create your tasks here
 from __future__ import absolute_import, unicode_literals
 from celery import shared_task
+from confluence.settings import FACEBOOK_PAGE_ACCESS_TOKEN
 import facebook
 
 
-@shared_task
+@shared_task(name='POST_TO_FACEBOOK')
 #message=input("Please enter the message to be displayed: ")
-def post_to_fb_page(message_entered,link):
-    graph = facebook.GraphAPI(access_token='EAAHZCnvXOZClgBAK7Llvam8SPZBPlrR8fffDmdeCBmmttUr09ZAep7NEOEh33ZC30UiQNEGUy5QZCDFi741TozPxMttZBtgmsnZAEzk364LU6co1BFN5vQbeAZBU262k7lz5CHbsMHVkAuFZC0HKSkbGB3GJgXdy5EX3giyZBUxAvR8MgZDZD')
+def post_to_fb_page(message,link):
+    graph = facebook.GraphAPI(access_token=FACEBOOK_PAGE_ACCESS_TOKEN)
     #message_entered=input("Please enter the message to be displayed: ")
     attachment =  {
-    #'name': name,
     'link': link,   #must be same as picture
-    #'caption': caption,
-    #'description': 'desc',
     'picture': link  #must be same as link
     }
-    graph.put_wall_post(message=message_entered, attachment=attachment)
-    return 'posted'
+    graph.put_wall_post(message=message, attachment=attachment)
+    

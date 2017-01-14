d0439165c749d32672128bd7e71b42ac521df6bf
shweta <sumanshweta44@gmail.com>
2017-01-15 01:20:07 +0530

    Celery task for tweets to twitter

--- src/social_media/tasks.py
+++ src/social_media/tasks.py
@@ -3,17 +3,24 @@ from __future__ import absolute_import, unicode_literals
 
 # Import secret tokens from settings.
 from confluence.settings import FACEBOOK_PAGE_ACCESS_TOKEN
-from confluence.settings import TWITTER_PAGE_ACCESS_TOKEN
+from confluence.settings import TWITTER_CONSUMER_KEY
+from confluence.settings import TWITTER_CONSUMER_SECRET
+from confluence.settings import TWITTER_ACCESS_KEY
+from confluence.settings import TWITTER_ACCESS_SECRET
+
+# Import shared tasks
 from celery import shared_task
 
 # Import GraphAPI for posting attachment to facebook page.
 from facebook import GraphAPI
-from TwitterAPI import TwitterAPI
-
-api = TwitterAPI(CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN_KEY, ACCESS_TOKEN_SECRET)
-
+# Import tweepy and requests for twitter posts.
+import os
+import tweepy
+import requests
 
 # Create your tasks here
+
+
 @shared_task(name='social_media.tasks.post_to_facebook')
 def post_to_facebook(message, link=None):
     """Posts a message to the Facebook page using GraphAPI authenticated via
@@ -36,20 +43,35 @@ def post_to_facebook(message, link=None):
     graph.put_wall_post(message=message, attachment=attachment)
 
 
-@shared_task(name='social_media.tasks.post_to_twitter')
-def post_to_twitter(status, path=None):
+@shared_task(name='social_media.tasks.tweeet_to_twitter')
+def tweet_to_twitter(message, url=None):
     """Posts a message to the Twitter page using TwitterAPI authenticated via
-       `TWITTER_PAGE_ACCESS_TOKEN`.
+       TWITTER_CONSUMER_KEY, TWITTER_CONSUMER_SECRET, TWITTER_ACCESS_KEY and
+       TWITTER_ACCESS_SECRET.
 
        Args:
-           - status: str. The content of the message to be posted on Twitter.
-           - path: The path of the image from local system.
+           - message: str. The content of the message to be posted on Twitter.
+           - link: (Optional) Url of the attachment to be posted along
+             with message.
 
        Returns:
            - None
 
     """
-    file = open(path, 'rb')
-    data = file.read()
-    api.request('statuses/update_with_media', {status: status}, {'media[]': data})
-    print("posted")
+    auth = tweepy.OAuthHandler(TWITTER_CONSUMER_KEY, TWITTER_CONSUMER_SECRET)
+    auth.set_access_token(TWITTER_ACCESS_KEY, TWITTER_ACCESS_SECRET)
+    api = tweepy.API(auth)
+
+    if url is not None:
+        filename = 'image.jpg'
+        request = requests.get(url, stream=True)
+        if request.status_code == 200:
+            with open(filename, 'wb') as image:
+                for chunk in request:
+                    image.write(chunk)
+            api.update_with_media(filename, status=message)
+            os.remove(filename)
+        else:
+            print("Unable to download image")
+    else:
+        api.update_status(message)

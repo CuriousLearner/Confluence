567c848c6279bf8566b450bc47faa2565d462ddd
shweta <sumanshweta44@gmail.com>
2017-02-03 18:48:51 +0530

    Changes fixed as required

--- src/social_media/tasks.py
+++ src/social_media/tasks.py
@@ -1,19 +1,42 @@
 """Contains celery tasks to post messages on various social media platforms."""
 from __future__ import absolute_import, unicode_literals
 
+import os
+
 # Import secret tokens from settings.
 from confluence.settings import FACEBOOK_PAGE_ACCESS_TOKEN
-from confluence.settings import TWITTER_PAGE_ACCESS_TOKEN
+
+# Import shared tasks
 from celery import shared_task
 
 # Import GraphAPI for posting attachment to facebook page.
 from facebook import GraphAPI
-from TwitterAPI import TwitterAPI
 
-api = TwitterAPI(CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN_KEY, ACCESS_TOKEN_SECRET)
+# Import twitter_api and requests for twitter post
+import requests
+from .utils import twitter_api_authentication
+twitter_api = twitter_api_authentication()
+
+
+def save_image_from_url(image_url):
+    """Extract a image from the image_url
+
+       Args:
+           - image_url: Url of the attachment to be posted.
+       Returns:
+           - filename: Image saved in a file.
+
+    """
+    filename = 'image.jpg'
+    request = requests.get(image_url, stream=True)
+    with open(filename, 'wb') as image:
+        for chunk in request:
+            image.write(chunk)
+    return filename
 
 
 # Create your tasks here
+
 @shared_task(name='social_media.tasks.post_to_facebook')
 def post_to_facebook(message, link=None):
     """Posts a message to the Facebook page using GraphAPI authenticated via
@@ -36,20 +59,23 @@ def post_to_facebook(message, link=None):
     graph.put_wall_post(message=message, attachment=attachment)
 
 
-@shared_task(name='social_media.tasks.post_to_twitter')
-def post_to_twitter(status, path=None):
-    """Posts a message to the Twitter page using TwitterAPI authenticated via
-       `TWITTER_PAGE_ACCESS_TOKEN`.
+@shared_task(name='social_media.tasks.tweet_to_twitter')
+def tweet_to_twitter(message, image_url=None):
+    """Posts a message to the Twitter page using twitter_api, an instance for
+       twitter_api_authentication imported from utility.
 
        Args:
-           - status: str. The content of the message to be posted on Twitter.
-           - path: The path of the image from local system.
+           - message: str. The content of the message to be posted on Twitter.
+           - image_url: (Optional) Url of the attachment to be posted along
+              with message.
 
        Returns:
            - None
 
     """
-    file = open(path, 'rb')
-    data = file.read()
-    api.request('statuses/update_with_media', {status: status}, {'media[]': data})
-    print("posted")
+    if image_url is not None:
+        filename = save_image_from_url(image_url)
+        twitter_api.update_with_media(filename, status=message)
+        os.remove(filename)
+    else:
+        twitter_api.update_status(message)
